12547b78a82e49b56e292f053e7cb95b3a1ee343
WorthATon <11Worth4Me@gmail.com>
2022-11-14 21:32:41 -0500

    updates to green_snake class

--- snake-complete/snake/game/casting/green_snake.py
+++ snake-complete/snake/game/casting/green_snake.py
@@ -2,3 +2,17 @@ import constants
 
 from game.casting.snake import Snake
 from game.shared.point import Point
+
+class green_snake(Snake):
+    
+    """   A long limbless reptile.
+    
+    The responsibility of Snake is to move itself.
+
+    Attributes:
+        _points (int): The number of points the food is worth.
+    """
+    def __init__(self):
+        super().__init__()
+        self._segments = []
+        self._prepare_body()
